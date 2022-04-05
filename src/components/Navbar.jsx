940bdd36b09ccbfe5833a59d404429f576c84183
blake-hodges <tbhodges@gmail.com>
2022-04-05 16:54:32 +0800

    fix mobile menu dropdown positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     
 
     return (
-        <div className="w-full flex flex-col md:flex-row items-center absolute z-10 text-white bg-black bg-opacity-20 h-20 px-4">
+        <div className="w-full flex flex-col md:flex-row md:items-center absolute z-10 text-white bg-black bg-opacity-20 h-20 px-4">
             <div className="hidden md:block pb-2">
                 <h1>Temporado</h1>
             </div>
@@ -16,10 +16,10 @@ function Navbar() {
                 <li className="">Book a Trip</li>
                 <li className="">Become a Host</li>
             </div>
-            <div className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+            <div className="md:hidden z-10" onClick={() => setMenuOpen(!menuOpen)}>
                 {!menuOpen ? <MenuIcon className="h-10 w-10" /> :  <XIcon className="h-10 w-10" />}
             </div>
-            <div className={menuOpen ? "md:hidden w-screen bg-black" : "absolute left-[-100%]"}>
+            <div className={menuOpen ? "md:hidden w-screen bg-black absolute top-0 left-0" : "absolute left-[-100%]"}>
                 <ul className={`text-center w-full bg-slate-300`}>
                     <li className="border-b">Home</li>
                     <li className="border-b">Destinations</li>
