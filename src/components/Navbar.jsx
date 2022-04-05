22508834b2a5b2ece0d6418a3941e90275b5498e
blake-hodges <tbhodges@gmail.com>
2022-04-05 16:35:24 +0800

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import { MenuIcon, XIcon } from '@heroicons/react/solid';
-import MobileMenu from './MobileMenu';
 
 function Navbar() {
-    const [openMenu, setOpenMenu] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     
 
     return (
-        <div className="flex flex-col">
-            <div className="w-screen bg-black">
-                {openMenu ? 
-                <XIcon className="text-white h-10 w-10" onClick={() => setOpenMenu(!openMenu)} /> :
-                <MenuIcon className="text-white h-10 w-10" onClick={() => setOpenMenu(!openMenu)} /> }
-                {openMenu ? <MobileMenu /> : ""  }
+        <div className="flex flex-col md:flex-row items-center">
+            <div className="hidden md:block">
+                <h1>Temporado</h1>
+            </div>
+            <div className="hidden md:flex">
+                <li className="">Home</li>
+                <li className="">Destinations</li>
+                <li className="">Book a Trip</li>
+                <li className="">Become a Host</li>
+            </div>
+            <div className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+                {!menuOpen ? <MenuIcon className="h-10 w-10" /> :  <XIcon className="h-10 w-10" />}
+            </div>
+            <div className={menuOpen ? "md:hidden w-screen bg-black" : "absolute left-[-100%]"}>
+                <ul className={`text-center w-full bg-slate-300`}>
+                    <li className="border-b">Home</li>
+                    <li className="border-b">Destinations</li>
+                    <li className="border-b">Book a Trip</li>
+                    <li className="border-b">Become a Host</li>
+                </ul>
             </div>
         </div>
     )
